eecb9acb626db840e4b936b3b02b05c144643aac
Adrian Celis Torres <adrian.celis@grupotawa.com>
2024-02-23 00:59:38 -0500

    yaaa josssss

--- RombiBack/Program.cs
+++ RombiBack/Program.cs
@@ -128,10 +128,10 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
     {
         ValidateIssuer = false,
         ValidateAudience = false,
-        ValidateLifetime = true,
+        ValidateLifetime = false,
         ValidateIssuerSigningKey = true,
-        //ValidIssuer = builder.Configuration["JWT:Issuer"],
-        //ValidAudience = builder.Configuration["JWT:Audience"],
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidAudience = builder.Configuration["JWT:Audience"],
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
 
     };
